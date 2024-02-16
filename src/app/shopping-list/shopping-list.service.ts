b6039fb7b94c7d72ba3fc3923d187c5eb2cf1b33
Grzegorz <grzegorz_gorka@wp.pl>
2024-02-16 16:02:01 +0100

    Edited shopping-list and recipe-edit components

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -6,6 +6,7 @@ import { Ingredient } from "../shared/ingredient.model";
 export class ShoppingListService {
   
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     
     private ingredients:Ingredient [] = [
         new Ingredient('Apples', 5),
@@ -15,6 +16,9 @@ export class ShoppingListService {
 
     getIngredients(){
       return this.ingredients.slice();
+    }
+    getIngredient(index: number){
+      return this.ingredients[index]
     }
     addIngredient(ingredient: Ingredient){
       this.ingredients.push(ingredient);
@@ -26,4 +30,12 @@ export class ShoppingListService {
           this.ingredients.push(...ingredients);
           this.ingredientsChanged.next(this.ingredients.slice());
       }
+    updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+    deleteIngedient(index: number){
+      this.ingredients.splice(index, 1);
+      this.ingredientsChanged.next(this.ingredients.slice())
+    }
    }
