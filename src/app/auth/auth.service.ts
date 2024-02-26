2e75b05cb6a0224a9db0949232ee834fc6c4cc51
Grzegorz <grzegorz_gorka@wp.pl>
2024-02-26 13:11:05 +0100

    1

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { BehaviorSubject,  throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { User } from "./user.model";
 
 
 export interface AuthResponseData{
@@ -16,35 +17,48 @@ export interface AuthResponseData{
 
 @Injectable({providedIn: 'root'})
 export class AuthService{
+
+    user = new BehaviorSubject<User>(null);
+
     constructor(private http: HttpClient) {}
+
     signup(email: string, password: string, ){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCG2OGCCsa-UBvSW2RVJp9MVeGCGH-uYOo', 
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=', 
             {
                 email: email,
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError),tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+        })
+        );
     }
 
     login(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCG2OGCCsa-UBvSW2RVJp9MVeGCGH-uYOo',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
             {
                     email: email,
                     password: password,
                     returnSecureToken: true 
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError),tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+        }));
     }
-   
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+            const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+            const user = new User(email, userId, token, expirationDate);
+            this.user.next(user);
+    }
+
     private handleError (errorRes: HttpErrorResponse) {
         let errorMessage = 'An unknown error occured!';
                 if (!errorRes.error || !errorRes.error.error) {
                     return throwError(errorMessage);
                 }
-                console.log(errorRes.error.error.message)
                 switch (errorRes.error.error.message) {
-                    
                     case 'EMAIL_EXISTS':
                         errorMessage = 'This email exists already.';
                         break;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'app-header',
@@ -8,14 +10,26 @@ import { DataStorageService } from "../shared/data-storage.service";
     
 
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    isAuthenticated = false;
+    userSub: Subscription;
 
-    constructor (private dataStorageService: DataStorageService) {}
+    constructor (private dataStorageService: DataStorageService, private authService: AuthService) {}
+
+    ngOnInit(): void {
+       this.userSub = this.authService.user.subscribe(user =>{
+        this.isAuthenticated = !user ? false : true; /* !!user */
+        
+       });
+    }
 
     onSaveData(){
         this.dataStorageService.storeRecipes();
     }
     onFetchData(){
         this.dataStorageService.fetchRecipes().subscribe();
+    }
+    ngOnDestroy(){
+        this.userSub.unsubscribe();
     }
 }
